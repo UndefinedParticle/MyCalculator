1adfaef615d8ce5fa50d51c21c3bf95a5b91a751
chiNmoy <5gsanmoy@gmail.com>
2021-07-17 23:01:40 +0530

    MyCalculator

---
+++ app/src/main/java/com/example/mycalculator/Splash.java
@@ -0,0 +1,31 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread=new Thread(){
+            public void run(){
+                try {
+                    sleep(500);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }finally {
+                    Intent intent=new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+
+        };thread.start();
+    }
+}
